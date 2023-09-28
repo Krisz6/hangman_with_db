834e8105ac4977b969040efd787e85f34a16359a
Krisz6 <szalay.krisztian93@gmail.com>
2023-09-28 19:10:36 +0200

    hibás

---
+++ src/main/java/hu/progmatic/hangman_with_db/service/WordService.java
@@ -0,0 +1,26 @@
+package hu.progmatic.hangman_with_db.service;
+
+import hu.progmatic.hangman_with_db.model.Word;
+import hu.progmatic.hangman_with_db.repo.WordRepo;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+@Service
+public class WordService {
+    private WordRepo wordRepo;
+
+    public WordService(WordRepo wordRepo) {
+        this.wordRepo = wordRepo;
+    }
+    public List<Word> getAllWord(){
+        return wordRepo.findAll();
+    }
+    public Word getRandomWord(){
+        List<Word> words = getAllWord();
+        Collections.shuffle(words);
+        return words.get(0);
+    }
+}
